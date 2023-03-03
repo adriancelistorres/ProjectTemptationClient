d53875231110b7f6a3c57b0d3a50bf10caea72ae
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2023-03-03 12:53:50 -0500

    Maquetado de Carrito

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrito.component.css']
 })
 export class CarritoComponent {
+  incrementValue() {
+    var value = parseInt((<HTMLInputElement>document.getElementById('quantity')).value, 10);
+    value = isNaN(value) ? 1 : value;
+    value++;
+    console.log(value);
+    (<HTMLInputElement>document.getElementById('quantity')).value = value.toString();
+  }
 
+  decrementValue() {
+    var value = parseInt((<HTMLInputElement>document.getElementById('quantity')).value, 10);
+    value = isNaN(value) ? 1 : value;
+    value < 2 ? value = 1 : '';
+    value--;
+    console.log(value);
+    (<HTMLInputElement>document.getElementById('quantity')).value = value.toString();
+  }
 }
