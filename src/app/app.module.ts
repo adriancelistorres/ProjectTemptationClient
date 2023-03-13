a080edb5003a4a76f98bb3fd2f022ad0e28b3339
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2023-03-13 16:11:13 -0500

    Navbar avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CargarscriptService } from './services/cargarscript.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PipePipe } from './shared/pipe.pipe';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CarritoComponent,
     ProductosComponent,
     CollectionsComponent,
+    PipePipe,
   ],
   imports: [
     BrowserModule,
--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,25 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnChanges, SimpleChanges } from '@angular/core';
+
+
+interface Prueba{
+  image: String | any
+  precio: number | any
+  cantidad: number | any
+
+}
 
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
 })
+
 export class CarritoComponent {
-  incrementValue() {
-    var value = parseInt((<HTMLInputElement>document.getElementById('quantity')).value, 10);
-    value = isNaN(value) ? 1 : value;
-    value++;
-    console.log(value);
-    (<HTMLInputElement>document.getElementById('quantity')).value = value.toString();
+
+  prueba: Prueba[] = [
+    {image: "imagen grande.png", precio : 12.6, cantidad: 15},
+    {image: "imagen grande.png", precio : 15.9, cantidad: 31},
+    {image: "imagen grande.png", precio : 20.3, cantidad: 56},
+    {image: "imagen grande.png", precio : 30.5, cantidad: 20}
+  ]
+
+
+
+
+  incrementValue(index: number) {
+    this.prueba[index].cantidad++;
   }
 
-  decrementValue() {
-    var value = parseInt((<HTMLInputElement>document.getElementById('quantity')).value, 10);
-    value = isNaN(value) ? 1 : value;
-    value < 2 ? value = 1 : '';
-    value--;
-    console.log(value);
-    (<HTMLInputElement>document.getElementById('quantity')).value = value.toString();
+  decrementValue(index: number) {
+    console.log(index)
+    console.log(this.prueba[index].cantidad)
+    if (this.prueba[index].cantidad<= 1) {
+      this.prueba[index].cantidad = 1
+    } else {
+      this.prueba[index].cantidad--; 
+    }
+  }
+  handleInputChange(index: number, event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+      const inputValue = parseInt(inputElement.value);
+      this.prueba[index].cantidad = inputValue;
+
   }
 }
