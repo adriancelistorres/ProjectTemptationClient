bfedc09df1f18a2fc44110aed0fd4417bac3d0c9
Adrian Celis Torres <adriancelistorres184@gmail.com>
2023-03-19 19:01:51 -0500

    new menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//declare var tns;
+declare var tns:any;
 
 @Component({
   selector: 'app-menu',
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
-  /*constructor() {}
+  constructor() {
+    this.ngOnInit()
+  }
 
   ngOnInit(): void{
     setTimeout(()=>{
@@ -137,5 +139,5 @@ export class MenuComponent {
       });
 
     },500);
-  }*/
+  }
 }
