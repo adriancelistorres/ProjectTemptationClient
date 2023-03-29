bf68b49c56b5ef232d1ea9487c87863d7233c499
DyerMandujano <dyer0206@gmail.com>
2023-03-29 12:56:16 -0500

    Creacion de componente reclamos

---
+++ src/app/components/reclamos/reclamos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reclamos',
+  templateUrl: './reclamos.component.html',
+  styleUrls: ['./reclamos.component.css']
+})
+export class ReclamosComponent {
+
+}
